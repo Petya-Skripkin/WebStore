e8e8284c3b88f6d56098ee4ce753244a318bdbb4
Petya02 <sk.petya2002@mail.ru>
2020-10-22 12:44:29 +0600

    Создал компонент Button

---
+++ stories/components/inputs/button.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Highlight from 'react-highlight';
+
+import { storiesOf } from '@storybook/react';
+
+import Button from '../../../src/components/button';
+
+const ButtonDemo = () => {
+  return (
+    <Button onClick= {() => console.log("hello")}>Войти</Button>
+  )
+}
+
+storiesOf("Components/Button", module).add(
+  "Button",
+  () => (
+    <div>
+      <h1>Default</h1>
+      <ButtonDemo />
+      <br />
+      <br />
+      <Highlight className="javascript">
+        {`
+          <Button onclick= {console.log('hello')}>Войти</Button>
+        />
+      `}
+      </Highlight>
+    </div>
+  ),
+  {
+    info: {
+      propTables: [Button],
+      propTablesExclude: [Highlight, ButtonDemo]
+    }
+  }
+)
