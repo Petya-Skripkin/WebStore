86444233b17a560ffeb7112ccc0f2624c66ac684
Petya-Skripkin <sk.petya02@mail.ru>
2020-10-23 10:43:05 +0600

    Создал компонент Basket

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import { OuterWear, Modal } from 'components/.';
+import { Basket, Modal } from 'components/.';
 
 const Home = () => {
 
   const [count, setCount] = React.useState(0);
   const [open, setOpen] = React.useState(false);
+  const [close, setClose] = React.useState(false);
 
   return (
     <>
-      <OuterWear
+      <Basket
+        close={close}
         img="./shirt.png"
         title="Рубашка"
         day="2 дня"
@@ -18,14 +20,15 @@ const Home = () => {
         onPlus={() => setCount(count + 1)}
         onMinus={() => setCount(count > 0 ? count - 1 : count)}
         onOpenModalWindow={() => setOpen(!open)}
+        onClose={() => setClose(!close)}
       />
-      <Modal 
-        isOpen= {open}
-        onClose= {() => setOpen(!open)}
-        Children= {
+      <Modal
+        isOpen={open}
+        onClose={() => setOpen(!open)}
+        Children={
           <>
             <h1>Рубашка</h1>
-            <div>А вот так это делается просто берешь и делаешь 
+            <div>А вот так это делается просто берешь и делаешь
 и делать можно много а можно вообще не делать или делать и делать или не делать и не делать или делать но не сильно</div>
           </>
         }
